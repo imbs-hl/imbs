31b4380bb6bf9e1bf6e92982ca19f6182a75af5e
Arne Schillert <arne.schillert@imbs.uni-luebeck.de>
2015-02-07 21:23:19 +0100

    Bump version
    - Add read.sample()
    _ Fix test for verbatimize()

---
+++ R/read.sample.R
@@ -0,0 +1,16 @@
+
+##' Reads SNPTEST sample files, ignores the second line though
+##'
+##' @title Read SNPTEST sample file
+##' @param file Name of the sample file
+##' @return data.frame containing the data
+##' @export 
+
+read.sample <- function(file){
+  header <- read.table(file, as.is = TRUE, nrows = 1)
+  body <-  read.table(file, as.is = TRUE, skip = 2,
+                      header = FALSE)
+  names(body) <- header
+  body[body == -9] <- NA
+  body
+}
--- tests/testthat/test-verbatimize.R
+++ tests/testthat/test-verbatimize.R
@@ -1,10 +1,10 @@
 context("Check verbatimize")
 
 x <- c('foo', 'bar', 'buz')
-x_pasted <- '\\verb=foo=, \\verb=bar=, \\verb=buz'
+x_pasted <- '\\verb=foo=, \\verb=bar=, \\verb=buz='
 
-test_that("verbatimze", {
-  expect_that(verbatimze(x) == x_pasted, is_true())
+test_that("verbatimize", {
+  expect_that(verbatimize(x) == x_pasted, is_true())
   expect_that(length(verbatimize(x)), equals(1))
   expect_that(verbatimize(x), is_a('character'))
 })
