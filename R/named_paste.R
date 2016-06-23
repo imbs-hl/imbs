6908c973756b0a4a961b89010dc6bc4d1d4ccfe0
Arne Schillert <arne.schillert@imbs.uni-luebeck.de>
2016-06-23 09:15:31 +0200

    Add convert_csv2csv
    - Changes delimiters in csv files
    - Bump pkg version

--- R/named_paste.R
+++ R/named_paste.R
@@ -63,3 +63,30 @@ named_paste0 <- function(..., sep = "") {
 named_file_path <- function(..., sep = .Platform$file.sep) {
   named_paste(..., sep = sep)
 }
+
+
+#' Convert csv into csv2 files and vice versa. 
+#' Can cope with vectors of input files
+#' 
+#'@title Changes delimiters in csv files
+#'
+#' @param in_file Input csv file(s) [character]
+#' @param out_file Output csv file(s) [character]
+#' @param from Direction
+#'
+#' @return Nothing, invoked for side effect of writing new files
+#' @export
+#'
+#' @examples
+convert_csv2csv <- function(in_file, out_file, from = c('de', 'en')){
+    sep <- c(de = ';', en = ',')[from]  
+    dec <- c(de = ',', en = '.')[from]  
+    
+    dat_list <- lapply(in_files, read.table, 
+                  header = TRUE, as.is = TRUE,
+                  dec = dec, sep = sep)
+    lapply(dat_list, write.table, 
+           row.names = FALSE, col.names = TRUE,
+           dec = dec, sep = sep)
+
+}
