f72bff7437ae2745e31a24eea77a20a9c882000c
Arne Schillert <arne.schillert@imbs.uni-luebeck.de>
2016-07-29 10:46:21 +0200

    Fix bug.

--- R/plink.R
+++ R/plink.R
@@ -1,18 +1,28 @@
 
 #' Write SNP or sample IDs to file
-#'
+#' 
 #' @param ids Vector of SNP or Sample IDs
 #' @param file File name to write to
-#' @param sample Logical. Should be TRUE for samples and FALSE for SNPs
-#'
+#' @param update Logical, Default is FALSE. Skip writing file if it exists
+#'   
+#' @param sample Logical. Should be TRUE for samples (the default) and FALSE for
+#'   SNPs
+#'   
 #' @return NULL
 #' @export
-write_plink_ids <- function(ids, file, sample = TRUE){
+write_plink_ids <- function(ids, file, update, sample = TRUE) {
   if (sample) {
-      out <- cbind(ids)
+    out <- cbind(ids, ids)
   } else {
-      out <- ids
+    out <- ids
   }
-  write.table(out, file, row.names = FALSE, col.names = FALSE,
-              quote = FALSE)
+  if (update | !file.exists(file)) {
+    write.table(
+      out,
+      file,
+      row.names = FALSE,
+      col.names = FALSE,
+      quote = FALSE
+    )
   }
+}
