28ca2c4b4a302673af83af965d15e08c36ec2948
Damian Gola <gola@imbs.uni-luebeck.de>
2018-01-03 11:34:53 +0100

    add check_command tests

---
+++ tests/testthat/test-check_command.R
@@ -0,0 +1,25 @@
+
+context("checkCommand")
+
+test_that("checkCommand", {
+  
+  skip_on_os("windows")
+  
+  dummy_command <- "Kb1VL4bg1TaJ92cYqwNiz5nvZy96VfiFueDV9wjcU8gP54N0uJXZ0t1gSwewcLrhBTI47KIl74rzmoxAWxAACzjr8x0IiHQdRDND"
+  command <- "cd"
+  
+  expect_true(check_command(command))
+  expect_true(test_command(command))
+  expect_equal(assert_command(command), command)
+  
+  expect_string(check_command(dummy_command))
+  expect_equal(check_command(dummy_command), sprintf("%s: command not found", dummy_command))
+  expect_equal(check_command(1), "Must be of type 'string', not 'double'")
+  
+  expect_false(test_command(1))
+  expect_false(test_command(dummy_command))
+  
+  expect_error(assert_command(1))
+  expect_error(assert_command(dummy_command))
+  
+})
