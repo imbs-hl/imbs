811ffac15dc3afc0a9317f05d2ebf2925512f38d
Damian Gola <gola@imbs.uni-luebeck.de>
2019-01-28 15:19:36 +0100

    return Fst result

--- R/plink.R
+++ R/plink.R
@@ -2030,8 +2030,7 @@ plink_fst <- function(bfile, output.prefix,
       outlier_evecs = outlier_evec_list,
       outlier_evals = outlier_eval_list,
       outlier_logs = outlier_log_list,
-      evecs = evecs,
-      evals = evals,
+      fst = fst,
       ld_log = ld_log,
       fst_log = fst_log
     )
